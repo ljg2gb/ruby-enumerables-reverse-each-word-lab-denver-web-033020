8b413714aa82aab25d91f4fa1d8042589e63d8d8
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-16 15:46:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split(' ')
-  array.collect { |word| word.reverse }
-  array.join(" ")
+  reversed_words_array = array.collect { |word| word.reverse }
+  reversed_words_array.join(" ")
 end
 
 #reverse_each_word.collect(array) { |word| word.reverse }
